f6674d7bad20866638793d4aa69176f1d8db3f5c
jebw <jeb@jdwilkins.co.uk>
2018-12-24 10:34:22 +0000

    Added Session data experiment

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,12 @@ class PagesController < ApplicationController
   def show
     render template: "pages/#{params[:page]}"
   end
+  
+  def set_name
+    if params[:name].present?
+      session[:username] = params[:name]
+    end
+    
+    redirect_to '/pages/home'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/pages/:page", to: "pages#show"
+  post "/pages/set_name", to: 'pages#set_name'
 end
